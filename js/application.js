68745b50863f6d8feba3a61d1a323698f658154c
Faithsend <matt@sandfalls.com>
2015-07-08 16:32:55 -0500

    so much better with a library js file for markdown

--- js/application.js
+++ js/application.js
@@ -1,2 +1,22 @@
-$(document).ready(function() {
-})
+$(document).ready(function(){
+  new MarkdownWidget().initialize();
+});
+
+var MarkdownView = {
+  outputArea: function() { return $("#output") },
+  inputArea: function() { return $("#input") }
+};
+
+
+function MarkdownWidget() {
+}
+
+MarkdownWidget.prototype.parseInput = function(event) {
+  console.log(event.keyCode);
+  text = MarkdownView.inputArea().val();
+  MarkdownView.outputArea().html("<pre>" + markdown.toHTML(text) + "</pre>");
+}
+
+MarkdownWidget.prototype.initialize = function(source, destination){
+  MarkdownView.inputArea().on("keyup", this.parseInput)
+};
