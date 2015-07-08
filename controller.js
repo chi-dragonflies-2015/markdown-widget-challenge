b15f8b789670044c57ab15535afd3a2ac41cee78
Jason Pettus & Nick Siefken <pair+jasonpettus.nsiefken@devbootcamp.com>
2015-07-08 16:45:39 -0500

    Complete basic but flawed implementation of italics and bold; out of time to continue

--- controller.js
+++ controller.js
@@ -2,11 +2,20 @@ $(document).ready(function() {
   output = new Output();
   output.textArea().on('keyup', function(event) {
     var input = output.textArea().val();
-      var doubleAst = /(\*)(.+)(\*)/;
-      var italicYes = doubleAst.test(input);
-      if(italicYes){
-        input = output.makeItalic(input)
+      // var italicPattern = /\*(.+)\*/;
+      // var italicYes = italicPattern.test(input);
+
+      var boldPattern =/\*{2}.*\*{2}/;
+      var boldYes = boldPattern.test(input);
+
+      // if(italicYes){
+      //   input = output.makeItalic(input)
+      // };
+
+      if(boldYes){
+        input = output.makeBold(input)
       };
+
       output.outputPlain(input);
   });
 
--- models.js
+++ models.js
@@ -4,11 +4,15 @@ function Output() {
   this.outputPlain = function(input) {
     this.preview().html(input);
   }
+
   this.makeItalic = function(input){
     var step1 = input.replace( /\*/,"<i>");
     var step2 = step1.replace(/\*/,"</i>");
     return step2;
+  }
 
+  this.makeBold = function(input){
+    return input.replace(/\*{2}(.*)\*{2}/, "<b>$1</b>");
   }
 }
 
